aa87f7250f9163cdee01a851a8188ff985405580
kadekframa <dframa19@gmail.com>
2022-02-23 19:10:07 +0800

    Add Contact Us Logic using JavaScript. Tetap Semangat Berlatih!!!, Pantang Meyerah!!!

--- assets/automation.js
+++
@@ -1,8 +0,0 @@
-let counter = 1;
-setInterval(function(){
-    document.getElementById('radio' + counter).checked = true;
-    counter++;
-    if(counter > 4){
-        counter = 1;
-    }
-}, 5000);
---
+++ assets/myScript.js
@@ -0,0 +1,32 @@
+// Slider Gambar
+let counter = 1;
+setInterval(function(){
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+    if(counter > 4){
+        counter = 1;
+    }
+}, 5000);
+
+
+// // Contact Us
+
+
+let nama = document.getElementById("name-pengirim");
+let email =document.getElementById("email-pengirim");
+let komentar =document.getElementById("komentar-pengirim");
+
+function send(){
+
+    if(nama.value !== "" && email.value !== "" && komentar.value !== ""){
+        alert(`Hallo ${nama.value}.., Terimakasih, Pesan Anda Telah Terkirim !`);
+        confirm(`Kami akan mengkonfirmasi pesan Anda melalui e-mail ${email.value} yang telah Anda inputkan. ${nama.value} Tetap Semangat dan Stay Healthy !`);
+    }else{
+        alert("Silahkan Inputkan Data Diri dan Komentar Terlebih Dahulu !");
+    }
+    
+    nama.value = "";
+    email.value = "";
+    komentar.value = "";
+
+}
